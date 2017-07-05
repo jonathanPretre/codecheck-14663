e4b8b44b7e0be7a68057f2c16dcd64e6ae4e8630
jonathanPretre <jonathan.pretre91@gmail.com>
2017-07-05 18:17:00 +0900

    test

--- src/ApiSystemSolution/ApiSystem/Program.cs
+++ src/ApiSystemSolution/ApiSystem/Program.cs
@@ -15,29 +15,69 @@ namespace ApiSystem
             var page = new Uri("http://challenge-server.code-check.io/api/recursive/ask");
             string jsonString;
 
-            for (int i = 0; i < args.Length; i++)
-            {
-                string output = String.Format("argv[{0}]: {0}", i, args[i]);
-                Console.WriteLine(output);
-            }
+            //string[] tabtest = { "b0c2b89f-4862-4814-8728-ddb0b36076ba", "6" };
 
-            if (args[0] == null || args[1] == null)
+            int result;
+
+            try
             {
-                Console.WriteLine("codecheck CLI should fail with status code 1");
-            }
-            else
-            { 
-                using (WebClient client = new WebClient())
+                for (int i = 0; i < args.Length; i++)
                 {
-                    client.QueryString.Add("n", args[1]);
-                    client.QueryString.Add("seed", args[0]);
-                    client.Encoding = Encoding.UTF8;
-                    jsonString = client.DownloadString(page);
+                    string output = String.Format("args[{0}]: {0}", i, args[i]);
+                    Console.WriteLine(output);
                 }
 
-                dynamic result = JsonConvert.DeserializeObject(jsonString);
-                Console.WriteLine(result.result);
+                if (int.Parse(args[1]) == 0)
+                {
+
+                }
+
+                if (int.Parse(args[1]) == 2)
+                {
+
+                }
+
+                if (int.Parse(args[1]) % 2 == 0)
+                {
+                    result = recursive(int.Parse(args[1]));
+                }
+
+                else
+                {
+                    using (WebClient client = new WebClient())
+                    {
+                        client.QueryString.Add("n", args[1]);
+                        client.QueryString.Add("seed", args[0]);
+                        client.Encoding = Encoding.UTF8;
+                        jsonString = client.DownloadString(page);
+                    }
+                    dynamic json = JsonConvert.DeserializeObject(jsonString);
+                    result = json.result;
+                }
+                Console.WriteLine(result);
             }
+            catch (WebException ex)
+            {
+                if (ex.Status == WebExceptionStatus.ProtocolError && ex.Response != null)
+                {
+                    var resp = (HttpWebResponse)ex.Response;
+                    if (resp.StatusCode == HttpStatusCode.BadRequest) // HTTP 400
+                    {
+                        Console.WriteLine(HttpStatusCode.BadRequest);
+                    }
+                    
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static int recursive(int n)
+        {
+            if (n <= 0)
+                return n - 1;
+
+            else
+                return n - 1 + recursive(n - 1);
         }
     }
 }
