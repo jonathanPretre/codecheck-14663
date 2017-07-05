db8ee88c1d78803f217d9a538b3ae8199440db69
jonathanPretre <jonathan.pretre91@gmail.com>
2017-07-05 15:24:20 +0900

    add json nuget

--- src/ApiSystemSolution/ApiSystem/Program.cs
+++ src/ApiSystemSolution/ApiSystem/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,19 @@ namespace ApiSystem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(1);
+            var page = new Uri("http://challenge-server.code-check.io/api/recursive/ask");
+            string jsonString;
+
+            using (WebClient client = new WebClient())
+            {
+                client.QueryString.Add("n", "4");
+                client.QueryString.Add("seed", "b0c2b89f-4862-4814-8728-ddb0b36076ba");
+                client.Encoding = Encoding.UTF8;
+                jsonString = client.DownloadString(page);
+            }
+
+            dynamic result = JsonConvert.DeserializeObject(jsonString);
+            Console.WriteLine(result.result);
         }
     }
 }
